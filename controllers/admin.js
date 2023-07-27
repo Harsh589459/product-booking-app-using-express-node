bcab8047db7d10ed93dad30604e8e210b9ea4115
harsh589459 <hs589459@gmail.com>
2023-07-27 22:21:05 +0530

    delet and cart page completed mysql database is also connected

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null,title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
-  console.log("edti")
   if (!editMode) {
     return res.redirect('/');
   }
@@ -38,26 +37,22 @@ exports.getEditProduct = (req, res, next) => {
   });
 };
 
-
-exports.postEditProduct=(req,res,next)=>{
+exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
-  const updatedPrice=req.body.price;
+  const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(prodId,updatedTitle,updatedImageUrl,updatedDesc,updatedPrice);
-
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
   updatedProduct.save();
   res.redirect('/admin/products');
-}
-exports.deleteProductById=(req,res,next)=>{
-  const prodId=req.params.productId;
-  console.log(prodId);
-  const deleteProduct = new Product(prodId,null,null,null,null);
-  deleteProduct.delete();
-  res.redirect('/admin/products')
-
-}
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -67,4 +62,10 @@ exports.getProducts = (req, res, next) => {
       path: '/admin/products'
     });
   });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
 };
