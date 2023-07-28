88b42821d12d5c0806d084a033619313ddd930d6
harsh589459 <hs589459@gmail.com>
2023-07-28 16:06:02 +0530

    sequelize added

--- util/database.js
+++ util/database.js
@@ -1,11 +1,19 @@
-const mysql=require('mysql2')
+// const mysql=require('mysql2')
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-completee',
-    password:''
+// const pool = mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     database:'node-completee',
+//     password:''
 
-})
+// })
 
-module.exports=pool.promise();
+// module.exports=pool.promise();
+
+const Sequelize = require('sequelize')
+
+const sequelize= new Sequelize('node-completee','root','',{
+    dialect:'mysql', host:'localhost'
+});
+
+module.exports=sequelize;
